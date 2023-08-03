2dd06331dbe5bdc104d44c8a7c7e395c62ad64dd
Sadia-Islam-Shafina <124452339+Sadia-Islam-Shafina@users.noreply.github.com>
2023-08-03 22:43:29 +0600

    Add files via upload

---
+++ FinalSourceCode/PharmacyManagementSystem/Admin.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PharmacyManagementSystem
+{
+    public partial class Admin : Form
+    {
+        int flagTag = 0;
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        public Admin(string s)
+        {
+            InitializeComponent();
+            adminLabelText.Text = s;
+        }
+
+        public void showTable(string s)
+        {
+            string conString = "Data Source=DESKTOP-KU8ITD4\\SQLEXPRESS01;Initial Catalog=PBSdb;User ID=sa;Password=asdf1234";
+            string query = s;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = conString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitAdmin_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            Form1 admToLog = new Form1();
+            admToLog.Show();
+            this.Hide();
+        }
+
+        private void btnAddPharmacist_Click(object sender, EventArgs e)
+        {
+            NewPharmacist anf = new NewPharmacist();
+            anf.Show();
+            this.Hide();
+        }
+
+        private void btnAddAdmin_Click(object sender, EventArgs e)
+        {
+            NewAdmin cna = new NewAdmin();
+            cna.Show();
+            this.Hide();
+        }
+
+        private void btnResolveIssues_Click(object sender, EventArgs e)
+        {
+            string query = "select * from Issues";
+            showTable(query);
+        }
+
+        private void btnAdminList_Click(object sender, EventArgs e)
+        {
+            string query = "select Name, Post, Phone, Email from AdminInfo";
+            showTable(query);
+            flagTag = 1;
+        }
+
+        private void btnPharmacyList_Click(object sender, EventArgs e)
+        {
+            string query = "select OwnerName, NID, ShopName, Contact, Location, Email from PharmacistInfo";
+            showTable(query);
+            flagTag = 2;
+        }
+
+        private void btnUserList_Click(object sender, EventArgs e)
+        {
+            string query = "select Name, DOB, Mobile, Email, UserName from UserInfo";
+            showTable(query);
+            flagTag = 3;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (flagTag == 0)
+            {
+                MessageBox.Show("Select a table first", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (flagTag == 1)
+            {
+                string conString = "Data Source=DESKTOP-KU8ITD4\\SQLEXPRESS01;Initial Catalog=PBSdb;User ID=sa;Password=asdf1234";
+                SqlConnection con = new SqlConnection(conString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                string query = "select * from AdminInfo where Name=@name";
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@name", searchTextBox.Text);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (flagTag == 2)
+            {
+                string conString = "Data Source=DESKTOP-KU8ITD4\\SQLEXPRESS01;Initial Catalog=PBSdb;User ID=sa;Password=asdf1234";
+                SqlConnection con = new SqlConnection(conString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                string query = "select * from PharmacistInfo where ShopName=@shopname";
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@shopname", searchTextBox.Text);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (flagTag == 3)
+            {
+                string conString = "Data Source=DESKTOP-KU8ITD4\\SQLEXPRESS01;Initial Catalog=PBSdb;User ID=sa;Password=asdf1234";
+                SqlConnection con = new SqlConnection(conString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                string query = "select * from UserInfo where UserName=@username";
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@username", searchTextBox.Text);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Something is wrong");
+            }
+        }
+    }
+}
---
+++ FinalSourceCode/PharmacyManagementSystem/User.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace PharmacyManagementSystem
+{
+    partial class User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User));
+            this.exitUser = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReportIssues = new System.Windows.Forms.Button();
+            this.btnEmptyCart = new System.Windows.Forms.Button();
+            this.btnConfirmPurchase = new System.Windows.Forms.Button();
+            this.btnLogOutUser = new System.Windows.Forms.Button();
+            this.userText = new System.Windows.Forms.Label();
+            this.picBoxUser = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.exitUser)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitUser
+            // 
+            this.exitUser.Image = ((System.Drawing.Image)(resources.GetObject("exitUser.Image")));
+            this.exitUser.Location = new System.Drawing.Point(1349, 12);
+            this.exitUser.Name = "exitUser";
+            this.exitUser.Size = new System.Drawing.Size(19, 18);
+            this.exitUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.exitUser.TabIndex = 9;
+            this.exitUser.TabStop = false;
+            this.exitUser.Click += new System.EventHandler(this.exitUser_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnReportIssues);
+            this.panel1.Controls.Add(this.btnEmptyCart);
+            this.panel1.Controls.Add(this.btnConfirmPurchase);
+            this.panel1.Controls.Add(this.btnLogOutUser);
+            this.panel1.Controls.Add(this.userText);
+            this.panel1.Controls.Add(this.picBoxUser);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(313, 768);
+            this.panel1.TabIndex = 11;
+            // 
+            // btnReportIssues
+            // 
+            this.btnReportIssues.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnReportIssues.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReportIssues.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportIssues.ForeColor = System.Drawing.Color.Black;
+            this.btnReportIssues.Location = new System.Drawing.Point(41, 455);
+            this.btnReportIssues.Name = "btnReportIssues";
+            this.btnReportIssues.Size = new System.Drawing.Size(208, 47);
+            this.btnReportIssues.TabIndex = 7;
+            this.btnReportIssues.Text = "Report Issues";
+            this.btnReportIssues.UseVisualStyleBackColor = false;
+            this.btnReportIssues.Click += new System.EventHandler(this.btnReportIssues_Click);
+            // 
+            // btnEmptyCart
+            // 
+            this.btnEmptyCart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnEmptyCart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEmptyCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmptyCart.ForeColor = System.Drawing.Color.Black;
+            this.btnEmptyCart.Location = new System.Drawing.Point(41, 382);
+            this.btnEmptyCart.Name = "btnEmptyCart";
+            this.btnEmptyCart.Size = new System.Drawing.Size(208, 47);
+            this.btnEmptyCart.TabIndex = 6;
+            this.btnEmptyCart.Text = "Empty Cart";
+            this.btnEmptyCart.UseVisualStyleBackColor = false;
+            // 
+            // btnConfirmPurchase
+            // 
+            this.btnConfirmPurchase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnConfirmPurchase.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfirmPurchase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmPurchase.ForeColor = System.Drawing.Color.Black;
+            this.btnConfirmPurchase.Location = new System.Drawing.Point(41, 307);
+            this.btnConfirmPurchase.Name = "btnConfirmPurchase";
+            this.btnConfirmPurchase.Size = new System.Drawing.Size(208, 47);
+            this.btnConfirmPurchase.TabIndex = 5;
+            this.btnConfirmPurchase.Text = "Confirm Purchase";
+            this.btnConfirmPurchase.UseVisualStyleBackColor = false;
+            // 
+            // btnLogOutUser
+            // 
+            this.btnLogOutUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnLogOutUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogOutUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOutUser.ForeColor = System.Drawing.Color.Black;
+            this.btnLogOutUser.Location = new System.Drawing.Point(41, 617);
+            this.btnLogOutUser.Name = "btnLogOutUser";
+            this.btnLogOutUser.Size = new System.Drawing.Size(208, 47);
+            this.btnLogOutUser.TabIndex = 4;
+            this.btnLogOutUser.Text = "Log Out";
+            this.btnLogOutUser.UseVisualStyleBackColor = false;
+            this.btnLogOutUser.Click += new System.EventHandler(this.btnLogOutUser_Click);
+            // 
+            // userText
+            // 
+            this.userText.AutoSize = true;
+            this.userText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userText.Location = new System.Drawing.Point(125, 222);
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(53, 24);
+            this.userText.TabIndex = 1;
+            this.userText.Text = "User";
+            // 
+            // picBoxUser
+            // 
+            this.picBoxUser.Image = ((System.Drawing.Image)(resources.GetObject("picBoxUser.Image")));
+            this.picBoxUser.Location = new System.Drawing.Point(53, 47);
+            this.picBoxUser.Name = "picBoxUser";
+            this.picBoxUser.Size = new System.Drawing.Size(196, 160);
+            this.picBoxUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxUser.TabIndex = 0;
+            this.picBoxUser.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(309, 224);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(529, 544);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.Color.White;
+            this.dataGridView2.Location = new System.Drawing.Point(844, 224);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(539, 544);
+            this.dataGridView2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(531, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Inventory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1088, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 24);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "My Cart";
+            // 
+            // User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1380, 768);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.exitUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "User";
+            this.Text = "User";
+            ((System.ComponentModel.ISupportInitialize)(this.exitUser)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox exitUser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReportIssues;
+        private System.Windows.Forms.Button btnEmptyCart;
+        private System.Windows.Forms.Button btnConfirmPurchase;
+        private System.Windows.Forms.Button btnLogOutUser;
+        private System.Windows.Forms.Label userText;
+        private System.Windows.Forms.PictureBox picBoxUser;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
